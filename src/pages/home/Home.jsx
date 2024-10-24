f45edf26552aeff028ebb8869ce0fc1dd3c77d9a
yunrap <dbsdpwl7@gmail.com>
2024-10-25 00:56:30 +0900

    init. 프로젝트 기본셋팅

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>home 화면입니다.</div>;
+};
+
+export default Home;
---
+++ src/pages/modify/Modify.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Modify = () => {
+  return <div>modify 화면입니다.</div>;
+};
+
+export default Modify;
