add9f37943a8521bdeb7606f39f6e5bbaaa0e54a
ksj <naver.daum0711@gmail.com>
2024-10-25 16:59:13 +0900

    Add color palette

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Color.css";
 
 const Detail = () => {
   return <div>detail 화면입니다.</div>;
--- src/pages/modify/Modify.jsx
+++ src/pages/modify/Modify.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Color.css";
 
 const Modify = () => {
   return <div>modify 화면입니다.</div>;
