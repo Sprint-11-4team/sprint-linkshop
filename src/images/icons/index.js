e524b75f09490d78d8420553298bcb1cc0a7ced6
kimgwanho0206@gmail.com <kimgwanho0206@gmail.com>
2024-10-25 17:15:06 +0900

    todo 5 clear

---
+++ src/images/icons/index.js
@@ -0,0 +1,29 @@
+import bannerDesktop from "./banner-desktop.png";
+import bannerPhone from "./banner-phone.png";
+import bannerTablet from "./banner-tablet.png";
+import notFound from "./not-found.png";
+import back from "./back.png";
+import check from "./check.png";
+import close from "./close.png";
+import edit from "./edit.png";
+import filterBtn from "./filter-btn.png";
+import search from "./search.png";
+import share from "./share.png";
+import heartEmpty from "./heart-empty.png";
+import heartFill from "./heart-fill.png";
+
+export {
+  bannerDesktop,
+  bannerPhone,
+  bannerTablet,
+  notFound,
+  back,
+  check,
+  close,
+  edit,
+  filterBtn,
+  search,
+  share,
+  heartEmpty,
+  heartFill,
+};
