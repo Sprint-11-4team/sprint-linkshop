29d4938a9865b2e1d2ecba2d290fcd422ffa2681
Crazyupinc <jiminyee@crazyupinc.com>
2024-10-28 20:40:53 +0900

    검색인풋필드 구현

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,22 +1,20 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import Header from "../../components/common/Header";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Header from '../../components/common/Header';
+import SearchInput from '../../components/home/SearchInput';
 
 const Home = () => {
   const navigate = useNavigate();
   const handleButtonClick = () => {
-    navigate("/modify");
+    navigate('/modify');
   };
 
   return (
     <>
       <Header buttonName="생성하기" onButtonClick={handleButtonClick} />
+      <SearchInput />
       <div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <br></br>
         <a href="/detail">detail - 임시링크</a>
         <br />
         <a href="/modify">modify - 임시링크</a>
