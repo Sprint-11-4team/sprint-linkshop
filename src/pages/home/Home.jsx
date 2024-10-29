ce04b6fd6d07d4ca0f1e167669e476648e8ec8c8
yunrap <dbsdpwl7@gmail.com>
2024-10-29 11:38:30 +0900

    파일 import 누락수정

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../components/common/Header';
 import SearchInput from '../../components/home/SearchInput';
