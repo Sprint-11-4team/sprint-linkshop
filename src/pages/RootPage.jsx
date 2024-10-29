6bf2a8c0ff052507c9cd3c7fa8164c9a8a3092c0
yunrap <dbsdpwl7@gmail.com>
2024-10-29 18:12:33 +0900

    [STYLE] 루트페이지 기타링크추가

--- src/pages/RootPage.jsx
+++ src/pages/RootPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import ToastPopup from '../components/common/ToastPopup';
 import Modal from '../components/common/Modal';
 import Header from '../components/common/Header';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const RootPage = () => {
   const [openPopupOne, setOpenPopupOne] = useState(false);
@@ -28,35 +28,41 @@ const RootPage = () => {
         <br />
         <br />
         <br />
-        <br />
-        <a href="/detail">detail - 임시링크</a>
-        <br />
-        <a href="/modify">modify - 임시링크</a>
-      </div>
-      <div>
-        <br />
-        <br />
-        <br />
-        <button onClick={() => setOpenPopupOne(true)}>팝업 클릭 1</button>
-        <button onClick={() => setOpenPopupTwo(true)}>팝업 클릭 2</button>
-        <button onClick={() => setModalOpen(true)}>Open Modal</button>
-        <ToastPopup
-          isOpen={openPopupOne}
-          onClose={() => setOpenPopupOne(false)}
-          text="등록이 완료되었습니다."
-          isBtnOne={true}
-        ></ToastPopup>
-        <ToastPopup
-          isOpen={openPopupTwo}
-          onClose={() => setOpenPopupTwo(false)}
-          text="정말 삭제하시겠어요?"
-          onClick={handleOkClick}
-        ></ToastPopup>
-        <Modal
-          type="default"
-          isOpen={isModalOpen}
-          onClose={() => setModalOpen(false)}
-        ></Modal>
+        <p></p>
+        <div style={{ textAlign: 'center', paddingTop: '100px' }}>
+          <Link to="/list">list - 임시링크</Link>
+          <br />
+          <Link to="/detail">detail - 임시링크</Link>
+          <br />
+          <Link to="/modify">modify - 임시링크</Link>
+          <br />
+          <Link to="/create">create - 임시링크</Link>
+          <div>
+            <br />
+            <br />
+            <br />
+            <button onClick={() => setOpenPopupOne(true)}>팝업 클릭 1</button>
+            <button onClick={() => setOpenPopupTwo(true)}>팝업 클릭 2</button>
+            <button onClick={() => setModalOpen(true)}>Open Modal</button>
+            <ToastPopup
+              isOpen={openPopupOne}
+              onClose={() => setOpenPopupOne(false)}
+              text="등록이 완료되었습니다."
+              isBtnOne={true}
+            ></ToastPopup>
+            <ToastPopup
+              isOpen={openPopupTwo}
+              onClose={() => setOpenPopupTwo(false)}
+              text="정말 삭제하시겠어요?"
+              onClick={handleOkClick}
+            ></ToastPopup>
+            <Modal
+              type="default"
+              isOpen={isModalOpen}
+              onClose={() => setModalOpen(false)}
+            ></Modal>
+          </div>
+        </div>
       </div>
     </>
   );
