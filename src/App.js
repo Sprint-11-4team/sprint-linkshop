c765008b949841ba56cda4a5695c0ae013142054
yunrap <dbsdpwl7@gmail.com>
2024-10-30 22:23:57 +0900

    리액트 / 경로접근시 list로 바로이동

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Detail from './pages/detail/Detail';
 import Modify from './pages/modify/Modify';
 import Create from './pages/create/Create';
-import RootPage from './pages/RootPage';
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<RootPage />} />
+        <Route path="/" element={<Navigate to="/list" replace />} />
         <Route path="/list" element={<Home />} />
         <Route path="/detail" element={<Detail />} />
         <Route path="/modify" element={<Modify />} />
