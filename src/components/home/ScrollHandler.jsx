c8c30b138103aa2968b8d50a5b5c950da9e4e984
Crazyupinc <jiminyee@crazyupinc.com>
2024-11-02 14:44:54 +0900

    무한스크롤 구현

--- src/components/home/ScrollHandler.jsx
+++ src/components/home/ScrollHandler.jsx
@@ -1,23 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useCallback } from 'react';
 
-const ScrollHandler = ({ loadMore }) => {
-  useEffect(() => {
-    const handleScroll = () => {
-      if (
-        window.innerHeight + window.scrollY >=
-        document.body.offsetHeight - 5
-      ) {
-        loadMore();
+import LoadingSpinner from '../common/LoadingSpinner';
+
+// Loader 컴포넌트
+
+const InfiniteScrollList = ({ onLoadMore, loading }) => {
+  const observerRef = useRef();
+
+  const handleObserver = useCallback(
+    (entries) => {
+      const target = entries[0];
+      if (target.isIntersecting && !loading) {
+        onLoadMore();
       }
-    };
+    },
+    [loading, onLoadMore],
+  );
 
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [loadMore]);
+  useEffect(() => {
+    const option = { threshold: 1.0 };
+    const observer = new IntersectionObserver(handleObserver, option);
 
-  return null;
+    if (observerRef.current) observer.observe(observerRef.current);
+
+    return () => observer.disconnect();
+  }, [handleObserver]);
+
+  return (
+    <div>
+      {loading && <LoadingSpinner />}
+      <div ref={observerRef} style={{ height: '1px' }} />
+    </div>
+  );
 };
 
-export default ScrollHandler;
+export default InfiniteScrollList;
