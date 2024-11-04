53ad2ee48605afa8222f9180962841c582d176fd
kimgwanho0206@gmail.com <kimgwanho0206@gmail.com>
2024-11-04 15:39:08 +0900

    토스트 컴포넌트 modify 페이지에서 사용

--- src/pages/modify/Modify.jsx
+++ src/pages/modify/Modify.jsx
@@ -1,58 +1,39 @@
 import React, { useState } from 'react';
 import { useNavigate, useParams, useLocation } from 'react-router-dom';
 import Header from '../../components/common/Header';
-// import CreateInput from '../../components/create/CreateInput';
 import AddButton from '../../components/create/AddButton';
-// import CreatePasswordButton from '../../components/create/CreatePasswordButton';
-import ModifyButton from '../../components/create/ModifyButton';
+import ModifyButton from '../modify/ModifyButton';
 import CreateProductInput from '../../components/create/CreateProductInput';
-// import MyproductList from './MyProductList';
 import ItemImgInput from '../../components/create/ItemImgInput';
 import CreateInput from '../../components/create/CreateInput';
 import CreatePasswordButton from '../../components/create/CreatePasswordButton';
 
 const Modify = () => {
-  // const [userInfo, setUserInfo] = useState({
-  //   name: '',
-  //   userId: '',
-  //   password: '',
-  // });
+  const [shopName, setShopName] = useState('');
+  const [Url, setUrl] = useState('');
 
-  // const handleInputChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setUserInfo((userInfo) => ({
-  //     ...userInfo,
-  //     [name]: value,
-  //   }));
-  // };
-  const [shopName, setShopName] = useState();
-  const [Url, setUrl] = useState();
-  const { id } = useParams();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const pwd = queryParams.get('pwd'); // pwd 값을 가져옵니다.
-  console.log(id + ' 확인' + pwd);
+  const pwd = queryParams.get('pwd');
 
+  const { id } = useParams();
   const navigate = useNavigate();
-  const handleButtonClick = () => {
-    navigate('/ ');
-  };
 
-  // const { name, userId, password } = userInfo;
+  const handleButtonClick = () => {
+    navigate('/');
+  };
 
   return (
     <div>
       <Header buttonName="돌아가기" onButtonClick={handleButtonClick} />
       <form className="form-body">
         <div className="create-input-wrapper">
-          {/* <div className="create-input-package-wrapper"> */}
           <div className="create-input-title">
             <h3>대표 상품</h3>
             <AddButton />
           </div>
           <CreateProductInput />
           <CreateProductInput />
-          {/* h3 대신 SectionTitle로? */}
           <h3 className="create-h3">내 쇼핑몰</h3>
           <div className="create-input-my-shop-wrapper">
             <ItemImgInput />
@@ -72,7 +53,9 @@ const Modify = () => {
             />
             <CreatePasswordButton />
           </div>
-          <ModifyButton />
+
+          {/* ModifyButton에 itemId로 id를 전달 */}
+          <ModifyButton itemId={id} pwd={pwd} />
         </div>
       </form>
     </div>
