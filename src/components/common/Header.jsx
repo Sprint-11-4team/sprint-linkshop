918bf59b1e23dfd453d507cf4174f8ea383fe920
Crazyupinc <jiminyee@crazyupinc.com>
2024-11-04 22:53:51 +0900

    기능수정

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { useLocation, Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+// import React from 'react';
+import { useLocation, useNavigate, Link } from 'react-router-dom';
 import './Header.css';
 import logo from '../../images/logo.png';
 
 const Header = ({ buttonName, onButtonClick }) => {
   const location = useLocation();
+  const navigate = useNavigate();
+  const [refreshTrigger, setRefreshTrigger] = useState(false);
 
   const handleLogoClick = (event) => {
     if (location.pathname === '/list') {
       event.preventDefault();
-      if (typeof window !== 'undefined') {
-        window.location.reload();
-      }
+      setRefreshTrigger(true);
     }
   };
+
+  useEffect(() => {
+    if (refreshTrigger) {
+      navigate('/temp', { replace: true }); // 임시 경로로 이동
+      setTimeout(() => {
+        navigate('/list', { replace: true }); // 원래 경로로 다시 이동
+      }, 0);
+      setRefreshTrigger(false);
+    }
+  }, [refreshTrigger, navigate]);
 
   return (
     <div>
