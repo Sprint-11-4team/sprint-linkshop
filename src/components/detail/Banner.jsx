60a007176481719643e4d6cafdd4e4a22feca115
chlwodudee <chlwodud895852@gmail.com>
2024-11-05 14:23:48 +0900

    배너백그라운드

--- src/components/detail/Banner.jsx
+++ src/components/detail/Banner.jsx
@@ -1,33 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./banner.css";
-import { bannerDesktop, bannerPhone, bannerTablet } from "../../images/icons";
 
 const Banner = () => {
-  const [bannerImage, setBannerImage] = useState("");
-
-  useEffect(() => {
-    const updateBannerImage = () => {
-      const width = window.innerWidth;
-
-      if (width >= 1240) {
-        setBannerImage(bannerDesktop);
-      } else if (width >= 744) {
-        setBannerImage(bannerTablet);
-      } else {
-        setBannerImage(bannerPhone);
-      }
-    };
-
-    updateBannerImage();
-
-    window.addEventListener("resize", updateBannerImage);
-
-    return () => window.removeEventListener("resize", updateBannerImage);
-  }, []);
-
   return (
     <div className="banner">
-      <img src={bannerImage} alt="상단 배너" className="banner-image" />
     </div>
   );
 };
